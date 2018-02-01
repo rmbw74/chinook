395d356dab1bcc233f397e4935abd73299a077a9
Ray Medrano <rmbw74@gmail.com>
2018-02-01 15:28:04 -0600

    renamed billing country to country

--- sales_per_country.sql
+++ sales_per_country.sql
@@ -1,4 +1,4 @@
-SELECT i.BillingCountry,  sum(i.Total) as "Total Sales"
+SELECT i.BillingCountry as "Country",  sum(i.Total) as "Total Sales"
 FROM Invoice as i 
 GROUP BY i.BillingCountry
 Order By "Total Sales" DESC;
