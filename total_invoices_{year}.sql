d17046e853a2309fbaa26e173ffa8266aeac1431
Ray Medrano <rmbw74@gmail.com>
2018-02-02 11:41:57 -0600

    modified query to show by year

--- total_invoices_{year}.sql
+++ total_invoices_{year}.sql
@@ -1,4 +1,6 @@
-SELECT COUNT(InvoiceId) as "Total Invoices 2009 and 2011"
+SELECT COUNT(InvoiceId) as "Total Invoices",
+strftime('%Y', invoiceDate) as InvoiceYear
 FROM Invoice 
 WHERE InvoiceDate between '2009-01-01' and '2009-12-31'
-OR InvoiceDate between '2011-01-01' and '2011-12-31';
+OR InvoiceDate between '2011-01-01' and '2011-12-31'
+group by InvoiceYear;
